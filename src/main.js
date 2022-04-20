420a7817c3037d326303ecc1360e1526c8d76d9f
Kamal Mango <kamal.mango7@gmail.com>
2022-04-20 23:31:17 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { store } from './store';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App';
+import AuthHandler from './components/AuthHandler';
+import ImageList from './components/ImageList';
+import UploadForm from './components/UploadForm';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(store);
+
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: ImageList },
+    { path: '/upload', component: UploadForm },
+    { path: '/oauth2/callback', component: AuthHandler }
+  ]
+});
+
+app.use(router);
+
+app.mount('#app');
